aeb35a554907deb85e0d1bf762772e1a049105ff
Administrator <Administrator@Jerry>
2018-09-17 14:41:35 +0800

    牵手西湖

---
+++ FH/src/org/xmgreat/test/TestCase.java
@@ -0,0 +1,40 @@
+package org.xmgreat.test;
+
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:applicationContext.xml" })
+public class TestCase
+{
+  /*
+   * 
+   * @Resource private AdminMapper adminMapper;
+   * 
+   * @Test public void sqlTest() {
+   * 
+   * // 通过条件查表获取SQL语句
+   * 
+   * RuleEntity RuleEntity =adminMapper.getSql(2);
+   * 
+   * String sql = RuleEntity.getSql();
+   * 
+   * //将获取的SQL语句传到xml文件进行查询
+   * 
+   * List<UserEntity> list = adminMapper.getAllList(sql);
+   * 
+   * for (int i = 0; i < list.size(); i++)
+   * 
+   * {
+   * 
+   * UserEntity UserEntity = list.get(i);
+   * System.out.println(UserEntity.getUserName());
+   * 
+   * }
+   * 
+   * }
+   * 
+   */
+
+}
